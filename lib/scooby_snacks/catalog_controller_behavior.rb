9643088fcabb1d4ffc2dc577298989b57b99b15d
Ned Henry <ethenry@ucsc.edu>
2018-02-27 11:35:35 -0800

    delegate solr naming to BlackLightConfiguration class

--- lib/scooby_snacks/catalog_controller_behavior.rb
+++ lib/scooby_snacks/catalog_controller_behavior.rb
@@ -4,29 +4,15 @@ module ScoobySnacks::CatalogControllerBehavior
 
     # Loop through all properties
     ScoobySnacks::METADATA_SCHEMA["properties"].each do |prop_name, prop|
-       
+      
       # some fields have a "type" that changes their solr name
       name_options = {}
-      name_options[:type] = prop["index_type"] if !prop["index_type"].nil?
-      prop_solr_name = solr_name(prop_name, prop["index_as"], name_options)
-      
+
+      prop_solr_name = ScoobySnacks::BlacklightConfiguration.prop_solr_name(prop_name, prop)
+
       # Add Facet if appropriate
       if prop["facet"].to_s == "true"
         config.add_facet_field solr_name(prop_name, :facetable), label: prop["label"], limit: prop["facet_limit"]
-      end
-      
-      # Add index fields
-      if prop["facet"].to_s != "false"
-        options = {label: prop["label"]}
-        options[:itemprop] = prop["index_itemprop"] if !prop["itemprop"].nil?
-        options[:if] = prop["index_if"] if !prop["index_if"].nil?
-        options[:itemprop] = prop["index_itemprop"] if !prop["index_itemprop"].nil?
-        options[:helper_method] = prop["index_helper_method"].to_sym if !prop["index_helper_method"].nil?
-
-        # link to facet search if facetable
-        options[:link_to_search] = solr_name(prop_name, :facetable) if prop["facet"].to_s == "true"
-
-        config.add_index_field prop_solr_name, options
       end
 
       # Add show field
@@ -51,3 +37,4 @@ module ScoobySnacks::CatalogControllerBehavior
     end
   end
 end
+
