e8371a541451f07ad2159e6af846cc8f9f219bbd
NedHenry <ethenry@ucsc.edu>
2020-01-24 15:20:11 -0800

    add ability to generate example metadata for each metadata field

--- lib/scooby_snacks/field.rb
+++ lib/scooby_snacks/field.rb
@@ -44,6 +44,17 @@ module ScoobySnacks
       end
     end
 
+    def example
+      return @raw_array["example"] if @raw_array["example"]
+      if controlled?
+        return "http://id.loc.gov/authorities/names/n2002034393"
+      elsif date?
+        return "01-01-1901"
+      else
+        return "Example #{name.titleize}"
+      end
+    end
+
     def controlled?
       return @controlled unless @controlled.nil?
       @controlled = false
